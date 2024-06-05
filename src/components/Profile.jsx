e9bdc69b76fa9e70dc119582884d86018892069a
Angst26 <ukozin36@gmail.com>
2024-06-05 11:41:31 +0300

    nothing to say

---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const Content = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://www.w-dog.ru/wallpapers/9/14/358044626273700/vesna-gory-alpy-dolina-doma-derevya-trava-zelen-nebo.jpg"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    new posts
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Content;
