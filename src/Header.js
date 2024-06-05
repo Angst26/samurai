324e91875600e18d3499b36de33f40a1ed8cc733
Angst26 <ukozin36@gmail.com>
2024-06-05 09:50:50 +0300

    начало

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <div>
+            ----it's Header----
+            <a href='#s'>Home</a>
+            <a href='#s'>News</a>
+            <a href='#s'>ewfqfwef</a>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/Technologies.js
@@ -0,0 +1,14 @@
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>html</li>
+                <li>css</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Technologies;
